7566f39202899beb60931be0fb7c171c9195e07e
LINKER <temp2temp@daum.net>
2022-05-03 17:12:33 +0900

    example code

---
+++ pspdfkit-react-example/src/App.js
@@ -0,0 +1,13 @@
+import PdfViewerComponent from './components/PdfViewerComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="PDF-viewer">
+        <PdfViewerComponent document={'Document.pdf'} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
